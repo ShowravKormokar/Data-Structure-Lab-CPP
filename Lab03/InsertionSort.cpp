718eaa373901c8c3cbce0693b1bf9d82447d35f0
ShowravKormokar <showravkormokar354@gmail.com>
2023-08-16 21:31:24 +0600

    Lab 4th, Learn Bubble sort, Selection sort & Insertion sort.

---
+++ Lab03/InsertionSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in an array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int item = arr[i];
+        int j = i - 1;
+        while (j >= 0 && item < arr[j])
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = item;
+    }
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
---
+++ Lab03/SelectionSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in an array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int small = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[small] > arr[j])
+                small = j;
+        }
+        if (small != i)
+        {
+            int temp = arr[small];
+            arr[small] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
