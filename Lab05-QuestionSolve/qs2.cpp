6c2a1a09d32aa55bfc1eb041166544a1aa6af8ff
ShowravKormokar <showravkormokar354@gmail.com>
2023-11-12 22:29:06 +0600

    Change 2nd question logicand make it more simple or efficient

--- Lab05-QuestionSolve/qs2.cpp
+++ Lab05-QuestionSolve/qs2.cpp
@@ -4,42 +4,31 @@ using namespace std;
 int main()
 {
     int n;
-    cout << "Enter the number: ";
+    cout << "Enter the number of elements in an array: ";
     cin >> n;
 
     int arr[n];
-    cout << "Enter the " << n << " elements: ";
+    cout << "Enter the " << n << " elements: " << endl;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    int largeNum1, largeNum2;
-
-    if (arr[0] > arr[1])
+    int firstLargeNum = 0, secondLargeNum = 0;
+    for (int i = 0; i < n; i++)
     {
-        largeNum1 = arr[0];
-        largeNum2 = arr[1];
-    }
-    else
-    {
-        largeNum1 = arr[1];
-        largeNum2 = arr[0];
-    }
-
-    // int max = arr[0];
-    for (int i = 2; i < n; i++)
-    {
-        if (arr[i] > largeNum1)
+        if (arr[i] > firstLargeNum)
         {
-            largeNum2 = largeNum1;
-            largeNum1 = arr[i];
+            secondLargeNum = firstLargeNum;
+            firstLargeNum = arr[i];
         }
-        else if (arr[i] > largeNum2 && arr[i] != largeNum1)
+        else if (arr[i] > secondLargeNum)
         {
-            largeNum2 = arr[i];
+            secondLargeNum = arr[i];
         }
     }
-    cout << "Large number1 = " << largeNum1 << endl;
-    cout << "Large number2 = " << largeNum2 << endl;
+
+    cout << "First largest number is: " << firstLargeNum << "\nSecond largest number is: " << secondLargeNum << endl;
+
+    return 0;
 }
